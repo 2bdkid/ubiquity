be21cdcf0c60d8ff41290e93cdd1101ed12d6e7f
Colin Watson <cjwatson@ubuntu.com>
2006-05-11 21:08:52 +0100

    0.99.78 (patches unapplied)
    Imported using git-ubuntu import.

--- ubiquity/frontend/gtkui.py
+++ ubiquity/frontend/gtkui.py
@@ -119,7 +119,9 @@ class Wizard:
         self.previous_partitioning_page = None
         self.installing = False
         self.returncode = 0
-        self.translations = get_translations()
+        self.language_questions = ('live_installer', 'welcome_heading_label',
+                                   'welcome_text_label', 'cancel', 'back',
+                                   'next')
         self.allowed_change_step = True
         self.allowed_go_forward = True
 
@@ -327,6 +329,14 @@ class Wizard:
 
 
     def translate_widgets(self):
+        if self.locale is None:
+            languages = []
+        else:
+            languages = [self.locale]
+        get_translations(languages=languages,
+                         core_names=['ubiquity/text/%s' % q
+                                     for q in self.language_questions])
+
         for widget in self.glade.get_widget_prefix(""):
             self.translate_widget(widget, self.locale)
 
@@ -343,20 +353,21 @@ class Wizard:
 
             # Ideally, these attributes would be in the glade file somehow ...
             name = widget.get_name()
+            textlen = len(text.encode("UTF-8"))
             if 'heading_label' in name:
                 attrs = pango.AttrList()
-                attrs.insert(pango.AttrScale(pango.SCALE_LARGE, 0, len(text)))
-                attrs.insert(pango.AttrWeight(pango.WEIGHT_BOLD, 0, len(text)))
+                attrs.insert(pango.AttrScale(pango.SCALE_LARGE, 0, textlen))
+                attrs.insert(pango.AttrWeight(pango.WEIGHT_BOLD, 0, textlen))
                 widget.set_attributes(attrs)
             elif 'extra_label' in name:
                 attrs = pango.AttrList()
-                attrs.insert(pango.AttrStyle(pango.STYLE_ITALIC, 0, len(text)))
+                attrs.insert(pango.AttrStyle(pango.STYLE_ITALIC, 0, textlen))
                 widget.set_attributes(attrs)
             elif name in ('drives_label', 'partition_method_label',
                           'mountpoint_label', 'size_label', 'device_label',
                           'format_label'):
                 attrs = pango.AttrList()
-                attrs.insert(pango.AttrWeight(pango.WEIGHT_BOLD, 0, len(text)))
+                attrs.insert(pango.AttrWeight(pango.WEIGHT_BOLD, 0, textlen))
                 widget.set_attributes(attrs)
 
         elif isinstance(widget, gtk.Button):
@@ -1011,8 +1022,7 @@ class Wizard:
             lang = self.language_choice_map[value][1]
             # strip encoding; we use UTF-8 internally no matter what
             lang = lang.split('.')[0].lower()
-            for widget in ('live_installer', 'welcome_heading_label',
-                           'welcome_text_label', 'cancel', 'back', 'next'):
+            for widget in self.language_questions:
                 self.translate_widget(getattr(self, widget), lang)
 
 
