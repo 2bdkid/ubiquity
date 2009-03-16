0fe0e3927e79a22a6112a7464d5ff9a0f59c79b2
Evan Dandrea <evand@ubuntu.com>
2009-03-16 17:02:01 +0000

    1.11.18 (patches unapplied)
    Imported using git-ubuntu import.

--- scripts/install.py
+++ scripts/install.py
@@ -1652,14 +1652,19 @@ exit 0"""
 
 
     def broken_packages(self, cache):
+        expect_count = cache._depcache.BrokenCount
+        count = 0
         brokenpkgs = set()
         for pkg in cache.keys():
             try:
                 if cache._depcache.IsInstBroken(cache._cache[pkg]):
                     brokenpkgs.add(pkg)
+                    count += 1
             except KeyError:
                 # Apparently sometimes the cache goes a bit bonkers ...
                 continue
+            if count >= expect_count:
+                break
         return brokenpkgs
 
     def do_install(self, to_install):
@@ -2067,9 +2072,9 @@ exit 0"""
         subarch = archdetect.communicate()[0].strip()
 
         if subarch.startswith('amd64/') or subarch.startswith('i386/') or subarch.startswith('lpia/'):
-            if 'grub' not in recorded:
+            if 'grub' not in keep:
                 difference.add('grub')
-            if 'lilo' not in recorded:
+            if 'lilo' not in keep:
                 difference.add('lilo')
 
         cache = Cache()
