d4f3a3d03b17dea6e6a9ade5669ba4cc4e6c1e22
Evan Dandrea <evand@ubuntu.com>
2009-09-24 09:40:12 -0700

    1.99.22 (patches unapplied)
    Imported using git-ubuntu import.

--- ubiquity/frontend/gtk_ui.py
+++ ubiquity/frontend/gtk_ui.py
@@ -48,6 +48,8 @@ import pygtk
 import gtk
 pygtk.require('2.0')
 import pango
+import warnings
+warnings.filterwarnings('ignore', 'error opening config file', pango.Warning)
 import gobject
 gobject.threads_init()
 import glib
@@ -779,7 +781,7 @@ class Wizard(BaseFrontend):
             self.quit_installer()
         else:
             step = self.step_name(self.steps.get_current_page())
-            if step.startswith("stepPart"):
+            if step == "partman":
                 print('dbfilter_handle_status stepPart')
                 self.set_current_page(self.steps.page_num(self.stepPartAuto))
             return False
@@ -791,6 +793,9 @@ class Wizard(BaseFrontend):
             if w in p.all_widgets:
                 return p.module.NAME
         return None
+
+    def page_name(self, step_index):
+        return self.steps.get_nth_page(step_index).get_name()
 
     def add_history(self, page, widget):
         history_entry = (page, widget)
@@ -913,6 +918,8 @@ class Wizard(BaseFrontend):
                     webview.open(slides)
                     self.slideshow_frame.add(webview)
                     webview.set_size_request(700, 420)
+                    webview.connect('new-window-policy-decision-requested',
+                                    self.on_slideshow_link_clicked)
                     self.slideshow_frame.show_all()
                 except ImportError:
                     fail = 'Webkit not present.'
@@ -1018,6 +1025,7 @@ class Wizard(BaseFrontend):
 
 
     # Callbacks
+
     def on_quit_clicked(self, widget):
         self.warning_dialog.show()
         response = self.warning_dialog.run()
@@ -1084,7 +1092,7 @@ class Wizard(BaseFrontend):
 
         self.allow_change_step(False)
 
-        step = self.step_name(self.steps.get_current_page())
+        step = self.page_name(self.steps.get_current_page())
 
         # Beware that 'step' is the step we're leaving, not the one we're
         # entering. At present it's a little awkward to define actions that
@@ -1110,17 +1118,14 @@ class Wizard(BaseFrontend):
 
         # setting actual step
         step_num = self.steps.get_current_page()
-        step = self.step_name(step_num)
+        step = self.page_name(step_num)
         syslog.syslog('Step_before = %s' % step)
 
         if step.startswith("stepPart"):
             self.previous_partitioning_page = step_num
         # Automatic partitioning
-        elif step == "stepPartAuto":
+        if step == "stepPartAuto":
             self.process_autopartitioning()
-        # Advanced partitioning
-        elif step == "stepPartAdvanced":
-            self.info_loop(None)
         # Identification
         elif step == "stepUserInfo":
             self.process_identification()
@@ -1190,7 +1195,7 @@ class Wizard(BaseFrontend):
         # Setting actual step
         step = self.step_name(self.steps.get_current_page())
 
-        if step == "stepReady":
+        if step == "summary":
             self.next.set_label("gtk-go-forward")
             self.translate_widget(self.next)
 
@@ -1202,6 +1207,13 @@ class Wizard(BaseFrontend):
             self.quit_main_loop()
 
 
+    def on_slideshow_link_clicked(self, view, frame, req, action, decision):
+        uri = req.get_uri()
+        decision.ignore()
+        subprocess.Popen(['sensible-browser', uri],
+                         close_fds=True, preexec_fn=drop_all_privileges)
+        return True
+
     def link_button_browser (self, button, uri):
         lang = self.locale
         lang = lang.split('.')[0] # strip encoding
@@ -1211,6 +1223,10 @@ class Wizard(BaseFrontend):
 
 
     def on_steps_switch_page (self, foo, bar, current):
+        if self.step_name(current) == 'usersetup':
+            # Disable the forward button if nothing has been entered on the
+            # usersetup page yet.
+            self.info_loop(None)
         self.current_page = current
         self.translate_widget(self.step_label)
         syslog.syslog('switched to page %s' % self.step_name(current))
